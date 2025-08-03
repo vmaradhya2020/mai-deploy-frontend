e7b75aa28c986f3ba872249199c0d47f39a389f0
Vishwaradhya <69788666+vmaradhya2020@users.noreply.github.com>
2025-08-03 12:21:35 +0530

    Update script.js
    added backend path - mai-deploy-gray.vercel.app

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ function sendMessage() {
         addMessage('user', message);
         input.value = '';
         
-        fetch('https://aideploymentmai.vercel.app/life_chat', {
+        fetch('https://mai-deploy-gray.vercel.app.vercel.app/life_chat', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
